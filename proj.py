d10b6f677a22cbb02fbb51b82a6623bae449b7e7
Lucas-Golinghorst <golinlg@iastate.edu>
2017-08-07 14:16:10 -0500

    Framework for restaurant management project

--- proj.py
+++ proj.py
@@ -1,19 +1,10 @@
 from Tkinter import *
 
 root = Tk()
+root.geometry("1600x800+0+0")
+root.title("Restaurant Management System")
 
-label_1 = Label(root, text="Name")
-label_2 = Label(root, text="Password")
-entry_1 = Entry(root)
-entry_2 = Entry(root)
-
-label_1.grid(row=0, sticky=E)
-label_2.grid(row=1, sticky=E)
-
-entry_1.grid(row=0, column=1)
-entry_2.grid(row=1, column=1)
-
-checkBox = Checkbutton(root, text="Keep me logged in")
-checkBox.grid(columnspan=2)
+Top = Frame(root, width=1600, bg="green", relief=SUNKEN)
+Top.pack(side=TOP)
 
 root.mainloop()
